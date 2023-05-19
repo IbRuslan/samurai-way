228de7de2e937f1791428032d4a4b2a54e0725eb
IbRuslan <ribragimov2003@gmail.com>
2023-05-19 21:22:12 +0500

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Header} from "antd/es/layout/layout";
+import {Technologies} from "./components/Technologies";
 
 function App() {
   return (
     <div className="App">
      Hello, samurai! Let's go!
+        <Header />
+        <Technologies />
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Header() {
+    return (
+        <div>
+            <a>Home</a>
+            <a>News Feed</a>
+            <a>Messages</a>
+        </div>
+    )
+}
+
